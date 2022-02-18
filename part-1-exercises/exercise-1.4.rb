d658c020f78f3f48d1a380b7c1a8dfcc24b4029a
sporkupine <coffeechief25@gmail.com>
2022-02-17 19:12:29 -0600

    finished exercise 1.4

---
+++ part-1-exercises/exercise-1.4.rb
@@ -0,0 +1,5 @@
+def names_of_my_pets (name_1, name_2, name_3, name_4)
+    puts "I have 4 pets. Their names are #{name_1}, #{name_2}, #{name_3}, and #{name_4}!"
+end
+
+names_of_my_pets("Henley", "Aria", "Layla", "Roxy")
