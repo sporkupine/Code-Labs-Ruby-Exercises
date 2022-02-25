ef3eaaaaeb94c25b4666ef280237e25a80937f05
sporkupine <coffeechief25@gmail.com>
2022-02-24 19:26:16 -0600

    finished exercise 3.2

--- part-3-exercises/exercise-3.2.rb
+++ part-3-exercises/exercise-3.2.rb
@@ -5,3 +5,22 @@
 # Print out each attribute dog holds.
 
 # Create an instance method called intro. When you call intro, it should print Hey, my name is Clifford. I'm red and I am a dog to the console. Use string interpolation to do this.
+
+class Animal
+  attr_accessor :name, :animal_type, :color
+
+  def initialize (name, animal_type, color)
+    @name = name
+    @animal_type = animal_type
+    @color = color
+  end
+
+  def intro
+    puts "Hey, my name is #{@name}! I'm a #{@animal_type} and I am #{@color}!"
+  end
+
+end
+
+dog = Animal.new("Clifford", "dog", "red")
+
+dog.intro
