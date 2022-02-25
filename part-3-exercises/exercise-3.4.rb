141815f5315b0ada3d05463f9a31f1c24b8a07e6
sporkupine <coffeechief25@gmail.com>
2022-02-24 19:41:30 -0600

    finished exercise 3.4

--- part-3-exercises/exercise-3.4.rb
+++ part-3-exercises/exercise-3.4.rb
@@ -5,3 +5,28 @@
 # Create a class method called print_all_people. 
 # Loop through people and print each name and age in the format of "Name: John Doe, Age: 54". 
 # Call this method to see if the results are correct.
+
+class Person
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@people.push(self)
+  end
+
+  @@people = []
+  def Person.print_all_people
+    @@people.each do |person|
+      puts "Name: #{person.name}, Age: #{person.age}."
+    end
+  end
+
+
+  Person.new("Jerry", 38)
+  Person.new("Connor", 28)
+  Person.new("Mike", 22)
+  Person.new("Estelle", 79)
+
+  Person.print_all_people
+end
