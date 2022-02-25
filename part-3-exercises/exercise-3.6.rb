f7f2f1a0936daa8e1e0151563706260944ff43c8
sporkupine <coffeechief25@gmail.com>
2022-02-24 20:00:40 -0600

    finished exercise 3.6

--- part-3-exercises/exercise-3.6.rb
+++ part-3-exercises/exercise-3.6.rb
@@ -1,3 +1,23 @@
 # Define a class called Person.
-# Define a module called traits. In the module, define a method called eat and print out "Eating".
-# Include the module in the Person methods. Allow each person instance to call these methods the module gives.
+# Define a module called traits. 
+# In the module, define a method called eat and print out "Eating".
+# Include the module in the Person methods. 
+# Allow each person instance to call these methods the module gives.
+
+module Traits
+  def eat
+    puts "Eating"
+  end
+end
+
+class Person
+  include Traits
+  def initialize
+
+  end
+end
+
+
+
+pete = Person.new
+pete.eat
